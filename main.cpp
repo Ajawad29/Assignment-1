280102dea8bf2b1a1095ab992c6b9b73e5d5af6c
Ajawad29 <74527049+Ajawad29@users.noreply.github.com>
2020-11-23 18:20:29 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,170 +1,92 @@
 #include <iostream>
 #include <string>
-#include<math.h>
 using namespace std;
-class Point{
+class HugeInteger{
 private:
-    int x;
-    int y;
-public:
-    Point(){
-        x=1;
-        y=1;
-    }
-    Point(int a,int b){
-        x=a;
-        y=b;
-    }
-
-    int getX() {
-        return x;
-    }
-
-    int getY() const {
-        return y;
-    }
-
-    void setY(int b) {
-        y = b;
-    }
-
-    void setX(int a) {
-        x = a;
-    }
-
-
-};
-class Rectangle{
-private:
-    Point p1;
-    Point p2;
-    Point p3;
-    Point p4;
-    float length;
-    float width;
-    char fill;
-    char border;
-    float square(int a){
-        float x;
-        x=a*a;
-        return x;
+    int num[40];
+    string x;
+    void init(){
+        for(int a=39;a>=0;a--){
+            num[a]=0;
+        }
     }
 public:
-    Rectangle(Point a,Point b,Point c,Point d){
-        p1=a;
-        p2=b;
-        p3=c;
-        p4=d;
-        length=0;
-        width=0;
-        fill='*';
-        border='*';
+    HugeInteger(){
+        init();
+    }
+    HugeInteger(string a){
+        setInteger(a);
 
     }
+    void setInteger(string b){
 
-    void setLength(){
-        float a;
-        a=sqrt(square(p1.getX()-p2.getX())+square(p1.getY()-p2.getY()));
-        length=a;
-        if (length<1 || length>25){
-            length=25;
-        }
-    }
+        x=b;
+        init();
+        int y;
+        y=x.length();
+        int z=y-1;
+        for (int i = 39; i>39-y ;i--){
+            num[i]=x[z]-48;
+            z=z-1;
 
-    float getLength(){
-        return length;
-    }
-
-    void setWidth(){
-        float b;
-        b=sqrt(square(p2.getX()-p3.getX())+square(p2.getY()-p3.getY()));
-        width=b;
-        if (width<1||width>25){
-            width=25;
         }
 
     }
-    float getWidth(){
-        return width;
-    }
-    float getArea(){
-        float area;
-        area=length*width;
-        return area;
-    }
-    float getPerimeter(){
-        float perimeter=2*(length+width);
-        return perimeter;
-    }
-    void check(){
-        if(length==width){
-            cout<<"\nTHIS IS A SQUARE."<<endl;
-        }
-        else{
-            cout<<"\nTHIS IS A RECTANGLE"<<endl;
-        }
-    }
-
-    char getFill() {
-        return fill;
-    }
-
-    void setFill(char a) {
-        fill = a;
-    }
-
-    char getBorder() {
-        return border;
-    }
-
-    void setBorder(char b) {
-        border = b;
-    }
-
     void display(){
-        int c;
-        for(int i=1;i<=width;i++){
-            c=1;
-            while(c<=length){
-               if(i==1 || i==width){
-                   cout<<getBorder();
-               }
-               else{
-                   if(c==1 || c==length){
-                       cout<<getBorder();
-                   }
-                   else{
-                       cout<<getFill();
-                   }
+        for (int i = 0; i < 40 ; i++) {
+            cout<<num[i];
 
-               }
-               c=c+1;
-            }
-            cout<<endl;
         }
     }
+    HugeInteger add(HugeInteger c) {
+        HugeInteger d;
+        for (int i = 39; i >= 0; i--) {
+            d.num[i] = d.num[i] + c.num[i] + num[i];
+            if (d.num[i] >= 10) {
+                d.num[i] = d.num[i] - 10;
+                d.num[i - 1] = d.num[i - 1] + 1;
+            }
+        }
+
+        return d;
+    }
+
+    HugeInteger subt(HugeInteger c){
+        HugeInteger d;
+        for (int i = 39; i >=0 ; i--) {
+            if(num[i]<c.num[i]){
+                num[i-1]=num[i-1]-1;
+                num[i]=num[i]+10;
+            }
+            d.num[i]=num[i]-c.num[i];
+
+        }
+
+
+        return d;
+    }
+
+
 
 
 };
 int main() {
-    Point a(6,6);
-    Point b(1,6);
-    Point c(1,1);
-    Point d(6,1);
+    HugeInteger a("1510");
+    HugeInteger b("99");
+    HugeInteger c;
+    a.display();
+    cout<<endl;
+    b.display();
+    cout<<endl;
+    c.display();
+    cout<<endl;
+    c=a.add(b);
+    c.display();
+    cout<<endl;
+    c=a.subt(b);
+    c.display();
 
-    Rectangle a1(a,b,c,d);
-    a1.setLength();
-    a1.setWidth();
-    float x;
-    float y;
-    x=a1.getArea();
-    y=a1.getPerimeter();
-    cout<<"area = "<<x<<endl;
-    cout<<"perimeter ="<<y<<endl<<endl;
-    a1.check();
-    a1.setBorder('@');
-    a1.setFill('#');
-    a1.display();
+
 
     return 0;
 }
