2603524d89b433a7b1ce385c103091774c2e01b6
Ajawad29 <74527049+Ajawad29@users.noreply.github.com>
2020-11-23 18:14:24 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,90 +1,416 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class HugeInteger{
+class OrderDetails;
+class Orders;
+class Items;
+class Items{
 private:
-    int num[40];
-    string x;
+    int itemId;
+    string name;
+    string price;
+    string description;
+    string createdAt;
+    string updatedAt;
+    string type;
+
+
+public:
+    Items(){
+        itemId=0;
+        name="";
+        price="0.0$";
+        description="";
+        createdAt="";
+        updatedAt="";
+        type="";
+
+    }
+    Items(int a,string b, string c,string d,string e,string f,string g){
+        itemId=a;
+        name=b;
+        price=c;
+        description=d;
+        createdAt=e;
+        updatedAt=f;
+        type=g;
+
+    }
+
+    int getItemId() const {
+        return itemId;
+    }
+
+    const string &getName() const {
+        return name;
+    }
+
+    const string &getPrice() const {
+        return price;
+    }
+
+    const string &getDescription() const {
+        return description;
+    }
+
+    const string &getCreatedAt() const {
+        return createdAt;
+    }
+
+    const string &getUpdatedAt() const {
+        return updatedAt;
+    }
+
+    const string &getType() const {
+        return type;
+    }
+
+    void setType(const string &type) {
+        Items::type = type;
+    }
+
+    void setUpdatedAt(const string &updatedAt) {
+        Items::updatedAt = updatedAt;
+    }
+
+    void setCreatedAt(const string &createdAt) {
+        Items::createdAt = createdAt;
+    }
+
+    void setDescription(const string &description) {
+        Items::description = description;
+    }
+
+    void setPrice(const string &price) {
+        Items::price = price;
+    }
+
+    void setName(const string &name) {
+        Items::name = name;
+    }
+
+    void setItemId(int itemId) {
+        Items::itemId = itemId;
+    }
+    void display(){
+        cout<<"\n\nItem Id : "<<itemId<<",  Name : "<<name<<",  Description : "<<description<<", Created At : "<<createdAt<<", Updated At : "<<updatedAt<<", Type:"<<type<<endl<<endl;
+        cout<<endl<<endl;
+
+    }
+
+};
+class OrderDetails{
+private:
+    int orderDetailId;
+    string quality;
+    Orders *orders;
+    Items **items;
+    int size;
     void init(){
-        for(int a=39;a>=0;a--){
-            num[a]=0;
+        for (int i = 0; i <size; ++i) {
+            items[i]=new Items();
+
         }
     }
 public:
-    HugeInteger(){
+    OrderDetails(){
+        orderDetailId=0;
+        quality="";
+        size=5;
+        items=new Items*[size];
         init();
     }
-    HugeInteger(string a){
-        setInteger(a);
-
-    }
-    void setInteger(string b){
-
-        x=b;
+    OrderDetails(int a, string b,int c){
+        orderDetailId=a;
+        quality=b;
+        size=c;
+        items=new Items*[size];
         init();
-        int y;
-        y=x.length();
-        int z=y-1;
-        for (int i = 39; i>39-y ;i--){
-            num[i]=x[z]-48;
-            z=z-1;
+    }
 
-        }
+    int getSize() const {
+        return size;
+    }
 
+    void setSize(int size) {
+        OrderDetails::size = size;
+    }
+
+
+    int getOrderDetailId() const {
+        return orderDetailId;
+    }
+
+    const string &getQuality() const {
+        return quality;
+    }
+
+    Orders *getOrders() const {
+        return orders;
+    }
+
+    Items **getItems() const {
+        return items;
+    }
+
+    void setItems(Items **items) {
+        OrderDetails::items = items;
+    }
+
+    void setOrders(Orders *orders) {
+        OrderDetails::orders = orders;
+    }
+
+    void setQuality(const string &quality) {
+        OrderDetails::quality = quality;
+    }
+
+    void setOrderDetailId(int orderDetailId) {
+        OrderDetails::orderDetailId = orderDetailId;
     }
     void display(){
-        for (int i = 0; i < 40 ; i++) {
-            cout<<num[i];
-
+        cout<<"ORDER DETAIL ID: "<<orderDetailId<<", ORDER QUALITY: "<<quality<<endl<<endl;
+        for (int i = 0; i < size; ++i) {
+            cout<<(i+1)<<".  ";
+            items[i]->display();
         }
     }
-    HugeInteger add(HugeInteger c) {
-        HugeInteger d;
-        for (int i = 39; i >= 0; i--) {
-            d.num[i] = d.num[i] + c.num[i] + num[i];
-            if (d.num[i] >= 10) {
-                d.num[i] = d.num[i] - 10;
-                d.num[i - 1] = d.num[i - 1] + 1;
-            }
-        }
-
-        return d;
-    }
-
-    HugeInteger subt(HugeInteger c){
-        HugeInteger d;
-        for (int i = 39; i >=0 ; i--) {
-            if(num[i]<c.num[i]){
-                num[i-1]=num[i-1]-1;
-                num[i]=num[i]+10;
-            }
-            d.num[i]=num[i]-c.num[i];
-
-        }
-
-
-        return d;
-    }
-
-
-
 
 };
+class Orders{
+private:
+    int orderId;
+    string description;
+    string createdAt;
+    string updatedAt;
+    string userName;
+    string pickUpTime;
+    string pickUpFrom;
+    string delieveryTime;
+    string delieverTo;
+    bool status;
+    OrderDetails **orderDetails;
+    int size;
+    void init(){
+        for (int i = 0; i <size; ++i) {
+            orderDetails[i]=new OrderDetails();
+
+        }
+    }
+public:
+    Orders(){
+        orderId=0;
+        createdAt="";
+        description="";
+        updatedAt="";
+        userName="";
+        pickUpTime="";
+        pickUpFrom="";
+        delieverTo="";
+        delieveryTime="";
+        status= true;
+        size=5;
+        orderDetails=new OrderDetails*[size];
+        init();
+
+
+    }
+    Orders(int oid,string des,string created,string updated,string user,string picktime,string pickfrom,string deliverto,bool s,int a,string delivertime){
+        orderId=oid;
+        createdAt=created;
+        description=des;
+        updatedAt=updated;
+        userName=user;
+        pickUpTime=picktime;
+        pickUpFrom=pickfrom;
+        delieverTo=deliverto;
+        delieveryTime=delivertime;
+        status=s;
+        size=a;
+        orderDetails=new OrderDetails*[size];
+        init();
+
+
+    }
+    int getOrderId() const {
+        return orderId;
+    }
+
+    const string &getDescription() const {
+        return description;
+    }
+
+    const string &getCreatedAt() const {
+        return createdAt;
+    }
+
+    const string &getUpdatedAt() const {
+        return updatedAt;
+    }
+
+    const string &getUserName() const {
+        return userName;
+    }
+
+    const string &getPickUpTime() const {
+        return pickUpTime;
+    }
+
+    const string &getPickUpFrom() const {
+        return pickUpFrom;
+    }
+
+    const string &getDelieveryTime() const {
+        return delieveryTime;
+    }
+
+    const string &getDelieverTo() const {
+        return delieverTo;
+    }
+
+    bool isStatus() const {
+        return status;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    void setSize(int size) {
+        Orders::size = size;
+    }
+
+    void setStatus(bool status) {
+        Orders::status = status;
+    }
+
+    void setDelieverTo(const string &delieverTo) {
+        Orders::delieverTo = delieverTo;
+    }
+
+    void setDelieveryTime(const string &delieveryTime) {
+        Orders::delieveryTime = delieveryTime;
+    }
+
+    void setPickUpFrom(const string &pickUpFrom) {
+        Orders::pickUpFrom = pickUpFrom;
+    }
+
+    void setPickUpTime(const string &pickUpTime) {
+        Orders::pickUpTime = pickUpTime;
+    }
+
+    void setUserName(const string &userName) {
+        Orders::userName = userName;
+    }
+
+    void setUpdatedAt(const string &updatedAt) {
+        Orders::updatedAt = updatedAt;
+    }
+
+    void setCreatedAt(const string &createdAt) {
+        Orders::createdAt = createdAt;
+    }
+
+    void setDescription(const string &description) {
+        Orders::description = description;
+    }
+
+    void setOrderId(int orderId) {
+        Orders::orderId = orderId;
+    }
+    OrderDetails **getOrderDetails() {
+        return orderDetails;
+    }
+
+    void setOrderDetails(OrderDetails **orderDetails) {
+        Orders::orderDetails = orderDetails;
+    }
+    void display(){
+        cout<<"\norder Id : "<<orderId<<",  Username : "<<userName<<",  Description : "<<description<<", Created At : "<<createdAt<<", Updated At : "<<updatedAt<<", Pick Up Time:"<<pickUpTime<<", Pick Up From : "<<pickUpFrom<<", Deliever To :"<<delieverTo<<", Delievery Time: "<<delieveryTime<<endl<<endl;
+        cout<<endl<<endl;
+        for (int i = 0; i < size; ++i) {
+            cout<<(i+1)<<".  ";
+            orderDetails[i]->display();
+        }
+    }
+
+};
+
 int main() {
-    HugeInteger a("1510");
-    HugeInteger b("99");
-    HugeInteger c;
-    a.display();
-    cout<<endl;
-    b.display();
-    cout<<endl;
-    c.display();
-    cout<<endl;
-    c=a.add(b);
-    c.display();
-    cout<<endl;
-    c=a.subt(b);
-    c.display();
+    Items *item = new Items(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A1(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A2(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A3(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A4(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A5(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A6(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A7(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A8(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    Items A9(1001,"s7 edge","200$","Phone","12-12-2012","10-10-2020","Electronic");
+    OrderDetails B1(001,"A+",2);
+    for (int i = 0; i < B1.getSize();i++) {
+        B1.getItems()[i]->setItemId(A1.getItemId());
+        B1.getItems()[i]->setName(A1.getName());
+        B1.getItems()[i]->setPrice(A1.getPrice());
+        B1.getItems()[i]->setDescription(A1.getDescription());
+        B1.getItems()[i]->setCreatedAt(A1.getCreatedAt());
+        B1.getItems()[i]->setUpdatedAt(A1.getUpdatedAt());
+        B1.getItems()[i]->setType(A1.getType());
+
+
+
+
+    }
+
+    OrderDetails B2(001,"A+",2);
+    for (int i = 0; i < B2.getSize();i++) {
+        B2.getItems()[i]->setItemId(A1.getItemId());
+        B2.getItems()[i]->setName(A1.getName());
+        B2.getItems()[i]->setPrice(A1.getPrice());
+        B2.getItems()[i]->setDescription(A1.getDescription());
+        B2.getItems()[i]->setCreatedAt(A1.getCreatedAt());
+        B2.getItems()[i]->setUpdatedAt(A1.getUpdatedAt());
+        B2.getItems()[i]->setType(A1.getType());
+
+
+
+
+    }
+
+    OrderDetails B3(001,"A+",2);
+    for (int i = 0; i < B3.getSize();i++) {
+        B3.getItems()[i]->setItemId(A1.getItemId());
+        B3.getItems()[i]->setName(A1.getName());
+        B3.getItems()[i]->setPrice(A1.getPrice());
+        B3.getItems()[i]->setDescription(A1.getDescription());
+        B3.getItems()[i]->setCreatedAt(A1.getCreatedAt());
+        B3.getItems()[i]->setUpdatedAt(A1.getUpdatedAt());
+        B3.getItems()[i]->setType(A1.getType());
+
+
+
+
+    }
+    Orders C1(101,"2012-series","1/1/2020","12/2/2020","A_Jawad","12pm","12-87-C driveway","11-A Johar town", false,5,"11am");
+    for (int j = 0; j <C1.getSize(); j++) {
+        C1.getOrderDetails()[j]->setOrderDetailId(B1.getOrderDetailId());
+        C1.getOrderDetails()[j]->setQuality(B1.getQuality());
+        for (int i = 0; i <B1.getSize() ; i++) {
+
+            C1.getOrderDetails()[i]->setItems(&item);
+
+        }
+
+    }
+    C1.display();
+
+
+
+
+
+
+
 
 
 
